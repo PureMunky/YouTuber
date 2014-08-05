403fc7a6df882902653c758aad37cd93022011a9
Phil Corbett <corbett.phil@gmail.com>
2014-08-05 13:08:04 -0400

    works for iframes now

--- youtuber.js
+++ youtuber.js
@@ -1,16 +1,25 @@
 (function () {
   var links = document.getElementsByTagName('a'),
-    youTubeURL = 'www.youtube.com/watch?v=',
+    frames = document.getElementsByTagName('iframe'),
+    frameYouTubeURL = 'www.youtube.com/embed/',
+    linkYouTubeURL = 'www.youtube.com/watch?v=',
     newYouTubeURL = 'www.youtube-nocookie.com/embed/',
     color = '#0F0',
     i = 0;
 
   for(i = 0; i < links.length; i++) {
-    if(links[i].href.indexOf(youTubeURL) > -1) {
-      links[i].href = links[i].href.replace(youTubeURL, newYouTubeURL);
+    if(links[i].href.indexOf(linkYouTubeURL) > -1) {
+      links[i].href = links[i].href.replace(linkYouTubeURL, newYouTubeURL);
       links[i].target = '_blank';
       links[i].style.border = '2px solid ' + color;
       links[i].style.backgroundColor = color;
+    }
+  }
+
+  for(i = 0; i < frames.length; i++) {
+    if(frames[i].src.indexOf(frameYouTubeURL) > -1) {
+      frames[i].src = frames[i].src.replace(frameYouTubeURL, newYouTubeURL);
+      frames[i].style.border = '2px solid ' + color;
     }
   }
 })();
