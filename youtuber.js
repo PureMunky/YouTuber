459482a1ca043788245a3fa52c2f07e7c339ec15
Phil Corbett <corbett.phil@gmail.com>
2014-08-12 08:23:35 -0400

    commenting

--- youtuber.js
+++ youtuber.js
@@ -1,12 +1,18 @@
+/* -- YouTuber -- */
+// Enables YouTube links and frames through some corporate firewalls.
+// Created By: Phil Corbett (@PureMunky)
+// Contribute: https://github.com/PureMunky/YouTuber
+
 (function () {
-  var links = document.getElementsByTagName('a'),
-    frames = document.getElementsByTagName('iframe'),
-    frameYouTubeURL = 'www.youtube.com/embed/',
-    linkYouTubeURL = 'www.youtube.com/watch?v=',
-    newYouTubeURL = 'www.youtube-nocookie.com/embed/',
+  var links = document.getElementsByTagName('a'),       // All links on the page.
+    frames = document.getElementsByTagName('iframe'),   // All frames on the page.
+    frameYouTubeURL = 'www.youtube.com/embed/',         // Root url for frames.
+    linkYouTubeURL = 'www.youtube.com/watch?v=',        // Root url for links.
+    newYouTubeURL = 'www.youtube-nocookie.com/embed/',  // Working root url for YouTube.
     color = '#0F0',
     i = 0;
 
+  // Iterate through the links and replace their href with the working root url.
   for(i = 0; i < links.length; i++) {
     if(links[i].href.indexOf(linkYouTubeURL) > -1) {
       links[i].href = links[i].href.replace(linkYouTubeURL, newYouTubeURL);
@@ -16,6 +22,7 @@
     }
   }
 
+  // Iterate through the frames and replace their src with the working root url.
   for(i = 0; i < frames.length; i++) {
     if(frames[i].src.indexOf(frameYouTubeURL) > -1) {
       frames[i].src = frames[i].src.replace(frameYouTubeURL, newYouTubeURL);
